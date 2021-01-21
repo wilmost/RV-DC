6374ed9be8b8dba1ddcc87ed2b2ff0d697792251
wilmost <wilmomartinezs@gmal.com>
2021-01-21 02:45:05 -0400

    inicializando repo

---
+++ visitante/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Visitante ,  Instituciones
+
+
+# Register your models here.
+admin.site.register(Visitante) 
+admin.site.register(Instituciones)
